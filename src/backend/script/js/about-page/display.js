3e317c9684ee41caedcf7e3dfbdb2d20e94811b2
StanlyDev <161256438+StanlyDev@users.noreply.github.com>
2024-08-01 01:26:12 -0600

    about display add

---
+++ src/backend/script/js/about-page/display.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const displaybtn = document.getElementById('displaycontent');
+    const resultcontent = document.querySelector('.result-content');
+    const aboutpage = document.querySelector('.about-page');
+
+    displaybtn.addEventListener('click', function() {
+        if (resultcontent.style.display === 'none') {
+            resultcontent.style.display = 'block';
+            aboutpage.style.display = 'none';
+        } else {
+            resultcontent.style.display = 'none';
+            aboutpage.style.display = 'block';
+        }
+    });
+});
